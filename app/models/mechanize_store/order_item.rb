6608f6cab5b455ff7c757dd09c7b1a869eac559a
Wilbert Ribeiro <wkelyson@gmail.com>
2014-04-20 11:51:37 -0300

    Implementing total method for order_item.

--- app/models/mechanize_store/order_item.rb
+++ app/models/mechanize_store/order_item.rb
@@ -2,5 +2,9 @@ module MechanizeStore
   class OrderItem < ActiveRecord::Base
     belongs_to :order
     belongs_to :product
+
+    def total
+        self.quantity.to_f * self.price.to_f
+    end
   end
 end
