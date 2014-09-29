2df409146a1565279df92678ab95f0d1a6ac1961
Rafael Uchôa <rafael@overmediacast.com>
2014-09-29 14:20:55 -0300

    Updates common_helper to work with submenus

--- app/controllers/mechanize_store/dashboards_controller.rb
+++ app/controllers/mechanize_store/dashboards_controller.rb
@@ -3,7 +3,7 @@ require_dependency "mechanize_store/application_controller"
 module MechanizeStore
   class DashboardsController < ApplicationController
     def show
-      active_menu :dashboard
+      active_menu :products
     end
   end
 end
